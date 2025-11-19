8d94040fc39beaa8d7866df4904303e3916297d1
aveW618 <146094921+aveW618@users.noreply.github.com>
2025-11-19 09:27:23 -0500

    checking for guest and company numbers, load files

---
+++ Conference.java
@@ -0,0 +1,52 @@
+import.java.io.companies;
+import.java.io.confGuests;
+public class Conference {
+	//load in external files
+	//check if there are no more than 10 people from each company
+	//check if there are no more than 100 people in attendance total
+	//for loop to iterate through all of the guests and find those guests of company 1 and then split them into a separate array
+		//then place them one by one in each of the table arrays
+			//repeat for the remaining companies
+	//add a user registration place
+		//use the Scanner function
+			//repeat the seating function above if the total number of guests is < than 100
+				//if there still exists !filled table arrays
+					//if the table arrays only have one person per company
+	n = 
+	String[] attendees = new String[1.5n];
+	
+	public void PreRegistration() throws IOException {
+		File companyFile = new File("companies.txt");
+		Scanner scan = new Scanner(companyFile);
+		String[] companiesPresent = new String[16];
+		
+		int i = 0;
+		while (scan.hasNext() && i < 16) {
+			String line = scan.NextLine();
+			companiesPresent[i] = line;
+			i++;
+		}
+		scan.close();
+		
+		File guestsFile = new File("confGuests.txt");
+		Scanner scan = new Scanner(guestsFile);
+		String[] guestsPresent = new String[100];
+		
+		int i = 0;
+		while (scan.hasNext() && i < 100) {
+			String line = scan.NextLine();
+			companiesPresent[i] = line;
+			i++;
+		}
+		scan.close();
+	}
+	
+	
+
+		
+	
+	
+	
+	
+	
+	
