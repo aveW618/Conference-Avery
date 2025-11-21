452ff9ee772f335267114d2f63e82d7dd032120b
aveW618 <146094921+aveW618@users.noreply.github.com>
2025-11-21 09:12:21 -0500

    registration files for new guests and new companies

---
+++ NewGuestNames.java
@@ -0,0 +1,10 @@
+public class NewGuestNames {
+	public static void main(String[] args) {
+		//getting user inputted guest name for people who register at the conference
+		Scanner getGuestName = new Scanner(System.in);
+		System.out.print("Enter Attendee Name (Format: last name,first name): ");
+		
+		//stores the user inputted guest name
+		String guestName = getGuestName.nextline();
+	}
+}
---
+++ newCompany.java
@@ -0,0 +1,10 @@
+public class newCompany {
+	public static void main(String[] args) {
+		//getting user inputted company name for companies who register at the conference
+		Scanner getCompanyName = new Scanner(System.in);
+		System.out.print("Enter Company Name: ");
+		
+		//stores the user inputted company name
+		String companyName = getCompanyName.nextline();
+	}
+}
