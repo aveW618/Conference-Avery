b0dbc710ad15757a1eeff019bd2072ae66efc2b9
aveW618 <146094921+aveW618@users.noreply.github.com>
2025-11-24 17:03:45 -0500

    Editing errors, putting attendee constructor, variable, and getter info in the separate Attendee file/class

---
+++ Attendee.java
@@ -0,0 +1,39 @@
+//Avery Wang
+//Conference project
+
+public class Attendee {
+	//declaring variables for attendee information
+	int userID;
+	String firstName;
+	String lastName;
+	int companyNumber;
+	
+	/* Constructor to create an Attendee object
+	 * takes user ID, first name, last name, company number, and user ID
+	 */
+	public Conference(int id, String f, String l, int c) {
+		userID = id;
+		firstName = f;
+		lastName = l;
+		companyNumber = c;
+	}
+	
+	/*
+	 * getters for the atrributes
+	 * returns the user ID, first name, last name, and company number
+	 */
+	public int getUserID() {
+		return userID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public int getCompanyNumber() {
+		return companyNumber;
+	}
+}
+	
+	
--- Conference.java
+++ Conference.java
@@ -6,16 +6,17 @@ import java.io.*;
 import java.util.*;
 
 public class Conference {
-	//check if there are no more than 10 people from each company
-	//check if there are no more than 100 people in attendance total
-	//for loop to iterate through all of the guests and find those guests of company 1 and then split them into a separate array
-		//then place them one by one in each of the table arrays
-			//repeat for the remaining companies
-	//add a user registration place
-		//use the Scanner function
-			//repeat the seating function above if the total number of guests is < than 100
-				//if there still exists !filled table arrays
-					//if the table arrays only have one person per company
+	/*check if there are no more than 10 people from each company
+	check if there are no more than 100 people in attendance total
+	for loop to iterate through all of the guests and find those guests of company 1 and then split them into a separate array
+		then place them one by one in each of the table arrays
+			repeat for the remaining companies
+	add a user registration place
+		use the Scanner function
+			repeat the seating function above if the total number of guests is < than 100
+				if there still exists !filled table arrays
+					if the table arrays only have one person per company
+	*/
 	
 	//declaring variables
 	int tables;
@@ -25,53 +26,28 @@ public class Conference {
 	int attendeesPerCompany;
 	int attendeeCounter;
 	
-	//declaring variables for attendee information
-	String firstName;
-	String lastName;
-	int companyNumber;
-	
-	//a constructor to create an Attendee object
-	public Conference(String f, String l, int c) {
-		firstName = f;
-		lastName = l;
-		companyNumber = c;
-	}
-	
-	//getters for the atrributes
-	public String getFirstName() {
-		return firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public int getCompanyNumber() {
-		return companyNumber;
-	}
-	
 	//delclaring an array to hold all attendees
 	Attendee[] items;
 	String filename = "confGuests.txt";
 	
-	//method to read in the guest data
-		//use throws IOException because the method may throw an exception that needs to be accounted for
+	/*
+	 * method to read in the guest data
+	 * use throws IOException because the method may throw an exception that needs to be accounted for
+	 * after reading in the guest array, creates a ID array with the attendee info
+	*/
 	public void readFile() throws IOException {
 		//reads in the confGuests file bto determine how many lines there are 
 			//to help with how big the guest array should be
 		File guestFile = new File(filename);
-		Scanner numLinesscan = new Scanner(guestFile);
+		Scanner numLinesScan = new Scanner(guestFile);
 		int numLines = 0;
 		
-		//skips over a header line if it exists
-		if (numLinesscan.hasNextLine()) {
-			numLinesscan.nextLine();
-		}
-		
 		//counts the number of lines in the file to help determine array size
-		while (numLinesscan.hasNextLine()) {
-			numLinesscan.nextLine();
+		while (numLinesScan.hasNextLine()) {
+			numLinesScan.nextLine();
 			numLines++;
 		}
-		numLinesscan.close();
+		numLinesScan.close();
 		
 		//arithmetic to help create an array with a 1.5 growth factor for in-person registrations
 		int arraySize = (int)(numLines * 1.5);
@@ -82,11 +58,6 @@ public class Conference {
 		Scanner scan = new Scanner(guestFile);
 		int i = 0;
 		
-		//skips header line if it exists
-		if (scan.hasNextLine()) {
-			scan.nextLine();
-		}
-		
 		//read each line of the guest file to create a 1D array of Attendee objects
 		while (scan.hasNextLine()) {
 			String line = scan.nextLine();
@@ -95,12 +66,13 @@ public class Conference {
 			String[] guestData = line.split(",");
 			
 			//stores the data from the split array
+			int userID = Integer.parseInt(guestData[0]);
 			String firstName = guestData[1];
 			String lastName = guestData[2];
 			int companyNumber = Integer.parseInt(guestData[3]);
 			
 			//create a new Attendee object and add it to the array for guests
-			Attendee a = new Attendee(firstName, lastName, companyNumber);
+			Attendee a = new Attendee(userID, firstName, lastName, companyNumber);
 			items[i] = a;
 			i++;
 		}
@@ -108,27 +80,51 @@ public class Conference {
 		scan.close();
 	}
 	
-	//method to manually register new attendees
-		//asks user for info and adds it to the guest array
+	/*
+	 * method to manually register new attendees
+	 * loops to get user info for registering new attendees until the array is full or the user stops adding attendees
+	 * adds the user's input to the guest array
+	 */
+	 
 	public void manualRegistration() {
-		//Scanner method to obtain user input
+		//obtaining user input
 		Scanner scan = new Scanner(System.in);
-		//prints out instructions and stores user input
-		System.out.println("Register New Attendee");
 		
-		System.out.print("First Name: ");
-		String firstName = scan.nextLine();
+		//sets a boolean for the later while loop 
+		boolean addingGuests = true;
 		
-		System.out.print("Last name: ");
-		String lastName = scan.nextLine();
+		while (addingGuests && attendeeCounter < items.length) {
+			//prints out instructions and stores user input
+			System.out.print("Would you like to register a new attendee? (Y/N) ");
+			String register = scan.nextLine();
+			
+			//if the user types N, change the boolean to false and exit the loop
+			if (register.equals("N")) {
+				addingGuests = false;
+			}
+			else if (register.equals("Y")) {
+				System.out.print("Attendee ID: ");
+				int userID = Integer.parseInt(scan.nextLine());
 		
-		System.out.print("Company number (1-16): ");
-		int companyNumber = Integer.parseInt(scan.nextLine());
+				System.out.print("First Name: ");
+				String firstName = scan.nextLine();
 		
-		//create a new Attendee object and add it to the guest array
-		Attendee a = new Attendee(firstName, lastName, companyNumber);
-		items[attendeeCounter] = a;
-		attendeeCounter++;
+				System.out.print("Last name: ");
+				String lastName = scan.nextLine();
+		
+				System.out.print("Company number (1-16): ");
+				int companyNumber = Integer.parseInt(scan.nextLine());
+		
+				//create a new Attendee object and add it to the guest array
+				Attendee a = new Attendee(userID, firstName, lastName, companyNumber);
+				items[attendeeCounter] = a;
+				attendeeCounter++;
+			}
+		}
+		//checks if the initial attendee array is full
+		if (attendeeCounter >= items.length) {
+			System.out.println("The conference is full. You can't add more attendees.");
+	}
 	}
 }
 
