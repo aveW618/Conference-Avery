af2f1738967b58d951f73be415047eb7e80b6509
aveW618 <146094921+aveW618@users.noreply.github.com>
2026-04-26 21:41:20 -0400

    minor changes in readability

--- Main.java
+++ Main.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
 
 public class Main {
 	//declaring instance variables
-    private static int TABLE_COUNT = 10;
-    private static int SEATS_PER_TABLE = 10;
-    private static int MAX_COMPANIES = 16;
-    private static int MAX_GUESTS = TABLE_COUNT * SEATS_PER_TABLE;
-    private static int MAX_GUESTS_PER_COMPANY = TABLE_COUNT;
+    private static int tableCount = 10;
+    private static int seatsPerTable = 10;
+    private static int maxCompanies = 16;
+    private static int maxGuests = tableCount * seatsPerTable;
+    private static int maxGuestsPerCompany = tableCount;
     
 	public static void main(String[] args) {
 		Conference c1 = new Conference();
