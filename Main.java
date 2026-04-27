16413eeaea1b0bb914e00e52e1dc965ee641f28f
aveW618 <146094921+aveW618@users.noreply.github.com>
2026-04-26 21:38:17 -0400

    working on main file, importing necessary tools and also declaring instance variables

--- Main.java
+++ Main.java
@@ -1,4 +1,19 @@
+//Avery Wang
+//April 26, 2026
+//Conference Project
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+	//declaring instance variables
+    private static int TABLE_COUNT = 10;
+    private static int SEATS_PER_TABLE = 10;
+    private static int MAX_COMPANIES = 16;
+    private static int MAX_GUESTS = TABLE_COUNT * SEATS_PER_TABLE;
+    private static int MAX_GUESTS_PER_COMPANY = TABLE_COUNT;
+    
 	public static void main(String[] args) {
 		Conference c1 = new Conference();
 		c1.readFile();
